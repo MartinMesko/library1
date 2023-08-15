d3161bb1139e96e961c3d5cbc6e1e569e8f48e7d
ladislavHubay <hubayladislav7@gmail.com>
2023-08-15 14:39:16 +0200

    upraveny test lekcia 15

---
+++ src/test/java/sk/itvkurze/lekcia_15/TitlesPage1Test.java
@@ -0,0 +1,21 @@
+package sk.itvkurze.lekcia_15;
+
+import org.junit.jupiter.api.Test;
+import sk.itvkurze.Lekcia_15.TitlesPage;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TitlesPage1Test {
+
+    @Test
+    void ifTheQuotedLinesFromTheSubfileAreNotIdenticalThanPrintTheText() throws IOException {
+        List<String> titles =  TitlesPage.loadTitlesFromFile("test.txt");
+
+        assertEquals("ABC", titles.get(0), "first line");
+        assertEquals("DEF", titles.get(1), "second line");
+        assertEquals("GHI", titles.get(2), "third line");
+    }
+}
