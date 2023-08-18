b7d347f3796c7d04b8ffe4b43d04cc3416f127d8
ladislavHubay <hubayladislav7@gmail.com>
2023-08-18 19:10:40 +0200

    pridane testy lekcia 16

--- src/test/java/sk/itvkurze/Lekcia_16/LibraryAppTest.java
+++ src/test/java/sk/itvkurze/Lekcia_16/LibraryAppTest.java
@@ -2,6 +2,11 @@ package sk.itvkurze.Lekcia_16;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LibraryAppTest {
@@ -29,5 +34,80 @@ public class LibraryAppTest {
         assertTrue(result, "Ukladanie DVD malo prebehnúť úspešne");
 
 
+    }
+
+    // testy true / false na validationCheckString()
+    @Test
+    public void testingValidationStringTrue() {
+        String input = "titles";
+        String actual = getString(input);
+
+        assertEquals("titles", actual);
+    }
+    @Test
+    public void testingValidationStringFalse() {
+        String input = "666\ntitles\n";
+        String actual = getString(input);
+
+        assertEquals("titles", actual);
+    }
+
+    private static String getString(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        TitlesPage titlesPage = new TitlesPage();
+        return titlesPage.validationCheckString();
+    }
+
+
+    // testy true / false na validationCheckInt()
+    @Test
+    public void testingValidationIntTrue() {
+        String input = "666\n";
+        int actual = getInt(input);
+
+        assertEquals(666, actual);
+    }
+
+    @Test
+    public void testingValidationIntFalse() {
+        String input = "titles\n666\n";
+        int actual = getInt(input);
+
+        assertEquals(666, actual);
+    }
+
+    private static int getInt(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        TitlesPage titlesPage = new TitlesPage();
+        return titlesPage.validationCheckInt();
+    }
+
+    // testy true / false na validationCheckISBN()
+    @Test
+    public void testingValidationISBNTrue() {
+        String input = "1234567891234\n";
+        String actual = getISBN(input);
+
+        assertEquals("1234567891234", actual);
+    }
+
+    @Test
+    public void testingValidationISBNFalse() {
+        String input = "titles\n1234567891234\n";
+        String actual = getISBN(input);
+
+        assertEquals("1234567891234", actual);
+    }
+
+    private static String getISBN(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        TitlesPage titlesPage = new TitlesPage();
+        return titlesPage.validationCheckISBN();
     }
 }
